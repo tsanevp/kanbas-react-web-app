b6b8ee6b428605004396e60e1d4ef84f848bc0d8
tsanevp <petertsanev7@gmail.com>
2024-09-10 14:31:32 -0700

    final deliverables complete

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 
 export default function TOC() {
-    return (
-      <ul>
-        <li><Link to="/Labs">Labs</Link></li>
-        <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-        <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-        <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-        <li><Link to="/Kanbas">Kanbas</Link></li>
-      </ul>
-    );
-  }  
+  return (
+    <ul>
+      <li><Link to="/Labs">Labs</Link></li>
+      <li><Link to="/Labs/Lab1">Lab 1</Link></li>
+      <li><Link to="/Labs/Lab2">Lab 2</Link></li>
+      <li><Link to="/Labs/Lab3">Lab 3</Link></li>
+      <li><Link to="/Kanbas">Kanbas</Link></li>
+      <li><a id="wd-github" href="https://github.com/tsanevp/kanbas-react-web-app" target="_blank" rel="noreferrer">Github Repo</a></li>
+    </ul>
+  );
+}  
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,6 +8,7 @@ export default function Labs() {
     return (
         <div id="wd-labs">
             <h1>Labs</h1>
+            <h2>Peter Tsanev</h2>
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Labs" />} />
