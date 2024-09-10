254851422a101889ea2b8dde613d0297a89e27ff
tsanevp <petertsanev7@gmail.com>
2024-09-09 19:16:48 -0700

    added alt text to imgs

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -155,11 +155,16 @@ export default function Lab1() {
                 <img id="wd-starship"
                     width="400px"
                     src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+                    alt="starship"
                 />
                 <br />
                 Loading a local image:
                 <br />
-                <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+                <img id="wd-teslabot" 
+                    src="images/teslabot.jpg" 
+                    height="200px" 
+                    alt="teslabot"
+                />
             </div>
             <div id="wd-forms">
                 <h4>Form Elements</h4>
