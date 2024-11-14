084bf8075ba4d98c4997a9e477e32fa90f2194ef
tsanevp <petertsanev7@gmail.com>
2024-11-13 19:41:09 -0800

    a5 progress

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -9,6 +9,7 @@ export default function TOC() {
       <li className="nav-item"><Link to="/Labs/Lab2" className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</Link></li>
       <li className="nav-item"><Link to="/Labs/Lab3" className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</Link></li>
       <li className="nav-item"><Link to="/Labs/Lab4" className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>Lab 4</Link></li>
+      <li className="nav-item"><Link to="/Labs/Lab5" className={`nav-link ${pathname.includes("Lab5") ? "active" : ""}`}>Lab 5</Link></li>
       <li className="nav-item"><Link to="/Kanbas" className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}>Kanbas</Link></li>
       <li className="nav-item"><a id="wd-github" href="https://github.com/tsanevp/kanbas-react-web-app/tree/a4" target="_blank" rel="noreferrer" className={`nav-link ${pathname.includes("Github") ? "active" : ""}`}>Github Repo</a></li>
     </ul>
