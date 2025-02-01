0dec78348e72437d89de2da5e68cd48b730b07cc
tsanevp <petertsanev7@gmail.com>
2025-02-01 11:01:46 -0800

    removed practice code

--- src/Labs/Lab3/TernaryOperator.tsx
+++
@@ -1,9 +0,0 @@
-export default function TernaryOperators() {
-   let loggedIn = true;
-   return (
-      <div id="wd-ternary-operator">
-         <h4>Logged In</h4>
-         {loggedIn ? <p>Welcome</p> : <p>Please login</p>} <hr />
-      </div>
-   );
-}
